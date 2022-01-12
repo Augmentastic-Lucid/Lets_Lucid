e5b6c53bba7395d9cda1628623dd1cb3d9bcccf9
Augmentastic-Lucid <97586595+Augmentastic-Lucid@users.noreply.github.com>
2022-01-12 21:13:14 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Routes from './routes';
-import ReactDOM from 'react-dom';
-import './style/index.scss';
-
-ReactDOM.render(<Routes/>, document.getElementById('root'));
